845f4642825730074cb9d55b5e07e753b1439ce4
guilhermeeec <guilhermepic2016@gmail.com>
2024-10-05 17:36:32 -0300

    Fix UDP socket thread not finishing bug

--- utils/udp_task_listener.py
+++ utils/udp_task_listener.py
@@ -8,7 +8,6 @@ class UdpTaskMessageListener:
         self.ip = ip
         self.port = self.get_random_available_port()
         self.handler = handler
-        self.running = False
         self.thread = None
 
     def get_random_available_port(self):
@@ -26,26 +25,42 @@ class UdpTaskMessageListener:
                 return False 
 
     def start(self):
-        self.running = True
+        self.exit_loop = False
         self.thread = threading.Thread(target=self.listen)
         self.thread.start()
 
     def stop(self):
         # Era pra essa flag liberar a thread do while, mas isso não funciona
-        self.running = False
+        self.send_finish_datagram_to_myself()
         if self.thread:
             self.thread.join()
+
+    def send_finish_datagram_to_myself(self):
+        tmp_send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        tmp_send_sock.sendto(bytes("END","utf-8"), (self.ip, self.port)) 
+        tmp_send_sock.close()
+
+    def process_received_datagram(self, data:bytes):
+        if not data:
+            return 
+        
+        try:
+            self.exit_loop = ( data.decode('utf-8') == "END" )
+        except UnicodeDecodeError:
+            pass
+        
+        if not self.exit_loop:
+            self.handler(data)
 
     def listen(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((self.ip, self.port))
         print(f"Listening on {self.ip}:{self.port}...")
 
-        while self.running:
+        while not self.exit_loop:
             try:
                 data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-                if data:
-                    self.handler(data)
+                self.process_received_datagram(data)
             except socket.error as e:
                 print(f"Socket error: {e}")
 
