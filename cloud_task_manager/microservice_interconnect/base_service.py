115b9002d62058b8502ba3f2c1bcf41e72a0f8db
guilhermeeec <guilhermepic2016@gmail.com>
2024-12-01 20:10:04 -0300

    Base service proptotype

--- cloud_task_manager/microservice_interconnect/base_service.py
+++ cloud_task_manager/microservice_interconnect/base_service.py
@@ -1,9 +1,11 @@
 import pika
 import json
-import threading
 import signal
 import jsonschema
-from typing import List, Dict, Any
+from typing import Dict, Any
+
+import pika.channel
+import pika.spec
 
 class BaseService:
     """
@@ -11,27 +13,11 @@ class BaseService:
     Other services are built as child of this one
     Implements registration and queue management for RabbitMQ
     """
+    def __init__(self) -> None:
+        self._is_running = False
+        self.func_name_to_func_and_schema_map = {}
 
-    def __init__(self, service_name: str, queue_names: List[str], ip='localhost') -> None:
-        """
-        Creates the service
-        using Pika
-
-        :param registry: service name
-        :param registry: list of queues names 
-        """
-        self.service_name = service_name
-        self.queue_names = queue_names
-        
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(ip)
-        )
-        self.channel = self.connection.channel()
-        self._is_running = True
-
-        self.func_name_to_schema_map = {}
-
-    def register_api_endpoint(self, func_name:str, schema: Dict[str, Any], func: Any):
+    def add_api_endpoint(self, func_name:str, schema: Dict[str, Any], func: Any):
         """
         Register a new API function execution uppon receiving certain JSON 
 
@@ -39,9 +25,21 @@ class BaseService:
         :param func: function to be executed 
         """
         self.func_name_to_func_and_schema_map[func_name] = {
-            "schema":schema,
-            "func":func
+            "func":func,
+            "schema":schema
         }
+
+    def _on_open(self, connection: pika.SelectConnection):
+        connection.channel(on_open_callback=self._on_channel_open)
+
+    def _on_channel_open(self, channel: pika.channel.Channel):
+        for func_name in self.func_name_to_func_and_schema_map.keys():
+            channel.queue_declare(queue=func_name)
+            channel.basic_consume(
+                queue=func_name,
+                on_message_callback=self._uppon_receiving_message,
+                auto_ack=False
+            )
 
     def _uppon_receiving_message(
         self,
@@ -59,7 +57,58 @@ class BaseService:
         :param body: message body
         """
 
-        response = self._process_generic_request(body)
+        response = self._process_generic_request(body, func_name=props.type)
+        self._send_response(ch, props, method,response)
+
+    def _process_generic_request(self, body:str, func_name:str) -> str:
+        """
+        Process received HTTP body, calling child processing function
+
+        :param body: HTTP request body as a string
+
+        :return: HTTP response body as a string
+        """
+        rcv_data = json.loads(body)
+        
+        try:
+            func_and_schema = self.func_name_to_func_and_schema_map[func_name]
+            jsonschema.validate(instance=rcv_data, schema=func_and_schema.get("schema"))
+            response = self._try_exec_child_func_and_build_response(func_and_schema.get("func"), rcv_data)
+        
+        except KeyError as e:
+            response = {
+                'status_code': 500,
+                'exception': f"Invalid RPC function: {e}"
+            }
+
+        except jsonschema.ValidationError as e: 
+            response = {
+                'status_code': 500,
+                'exception': f"Invalid message: {e}"
+            }
+        
+        except Exception as e:
+            response = {
+                'status_code': 500,
+                'exception': f"An unknown error occured: {e}"
+            }
+        
+        return json.dumps(response)
+
+    def _try_exec_child_func_and_build_response(self, func, argument):
+        try:
+            returned = func(argument)
+            return {"status_code":200,"return":returned}
+        except Exception as e:
+            return {"status_code":500,"exception":f"{e}"}
+
+    def _send_response(
+        self, 
+        ch:pika.channel.Channel, 
+        props:pika.spec.BasicProperties, 
+        method: pika.spec.Basic.Deliver, 
+        response:str
+    ):
         ch.basic_publish(
             exchange='',
             routing_key=props.reply_to,
@@ -70,85 +119,24 @@ class BaseService:
         )
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    def _process_generic_request(self, body: str) -> str:
-        """
-        Process received HTTP body, calling child processing function
-
-        :param body: HTTP request body as a string
-
-        :return: HTTP response body as a string
-        """
-        
-        response = {
-            'status_code': 500,
-            'error': "Function not implemented"
-        }
-
-        body[""]
-
-        for schema, child_func in zip(self.schemas_list, self.functions_list):
-            data = json.loads(body)
-            try:
-                jsonschema.validate(instance=data, schema=schema)
-                response = child_func(data)
-            except jsonschema.ValidationError as e: 
-                pass
-            except Exception as e:
-                response = {
-                    'status_code': 500,
-                    'error': str(e)
-                }
-        return json.dumps(response)
-
-    def _wait_and_process_requests(self) -> None:
-        """
-        Start the service
-        """
-        
-        print(f" [x] {self.service_name} awaiting RPC requests")
-        try:
-            while self._is_running:
-                self.connection.process_data_events(time_limit=1)
-        except Exception as e:
-            print(f"Error during processing: {e}")
-        finally:
-            if not self.connection.is_closed:
-                self.connection.close()
-
-    def _declare_and_consume_queue(self, func_name:str, func, ) -> None:
-        """
-        Declare and consume the queue wth the name of the function
-        """
-        self.channel.queue_declare(queue=func_name)
-        self.channel.basic_consume(
-            queue=func_name,
-            on_message_callback=self._uppon_receiving_message,
-            auto_ack=False
-        )
-
-    def stop(self) -> None:
-        """
-        Stop the service.
-        """
-        self._is_running = False
-        if not self.connection.is_closed:
-            self.connection.close()
-
-    def run(self) -> None:
-        """
-        Run the service in a separate thread with signal handling.
-        """
-        service_thread = threading.Thread(target=self._wait_and_process_requests)
-
+    def start(self):
         def signal_handler(sig: int, frame: Any) -> None:
             print('Stopping service...')
             self.stop()
-            service_thread.join()
             print('Service stopped.')
 
         signal.signal(signal.SIGINT, signal_handler)
         signal.signal(signal.SIGTERM, signal_handler)
 
-        self._declare_and_consume_queue()
+        self.connection = pika.SelectConnection(
+            pika.ConnectionParameters("localhost"),
+            on_close_callback=self._on_open
+        )
+        try: 
+            self.connection.ioloop.start()
+        except:
+            self.connection.close()
 
-        service_thread.start()
+    def stop(self):
+        if not self.connection.is_closed:
+            self.connection.close()
