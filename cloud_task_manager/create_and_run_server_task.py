c1cf6251e112c36c7008544593102c28b00c4b58
Fernando Dias <fernando.dias.3741@gmail.com>
2025-01-19 14:29:17 +0000

    Converted RPC calls to HTTP/POST calls

--- cloud_task_manager/create_and_run_server_task.py
+++ cloud_task_manager/create_and_run_server_task.py
@@ -1,5 +1,7 @@
 from microservice_interconnect.rpc_client import rpc_send, register_event
 import configparser
+import requests
+import time as tm
 
 if __name__ == "__main__":
 
@@ -9,19 +11,37 @@ if __name__ == "__main__":
     host = configs["server.broker"]["host"]
     port = int(configs["server.broker"]["port"])
 
-    register_event("Operator","register_task","Started registering a task",
-                   allow_registering=allow_register, host=host, port=port)
-    ret = rpc_send("rpc_exec_create_task",
-            {"task_id":"4fe5",
-            'host':'localhost',
-            'port':8080,
-            'username':'user',
-            'password':'123',
-            'files_paths':['client.py','task.py']
-            },
-            host=host,
-            port=port)
+    register_event("Operator","register_task","Started registering a task", allow_registering=allow_register, host=host, port=port)
+    startTime = tm.process_time_ns()
+    ret = requests.post(
+        f"http://localhost:{configs['server.gateway']['port']}/rpc_exec_create_task",headers={"Content-type":"Application/json"},
+        data="""{"task_id":"4fe5",
+"host":"localhost",
+"port":8080,
+"username":"user",
+"password":"123",
+"files_paths":["client.py","task.py"]
+}""")
     
-    register_event("Operator","start_task","Started starting a task",
-                   allow_registering=allow_register, host=host, port=port)
-    ret = rpc_send("rpc_exec_start_server_task",{"task_id":"4fe5"})
+    registerTime = tm.process_time_ns() - startTime
+    if ret.status_code != 200:
+        print("ERROR on request")
+        print(ret.content)
+        exit(1)
+    register_event("Operator","register_task", "Finished registering a task", allow_registering=allow_register, host=host, port=port)
+    register_event("Operator","operator_register_task_time", f"{registerTime}", allow_registering=allow_register, host=host, port=port)
+    
+
+    register_event("Operator","start_task","Started starting a task", allow_registering=allow_register, host=host, port=port)
+    startTime = tm.process_time_ns()
+    ret = requests.post(
+        f"http://localhost:{configs['server.gateway']['port']}/rpc_exec_start_server_task",headers={"Content-type":"Application/json"},
+        data="""{"task_id":"4fe5"}""")
+    if ret.status_code != 200:
+        print("ERROR on request")
+        print(ret.content)
+        exit(1)
+    processTime = tm.process_time_ns() - startTime
+    register_event("Operator","start_task","Finished starting a task", allow_registering=allow_register, host=host, port=port)
+    register_event("Operator","operator_register_task_time",f"{processTime}", allow_registering=allow_register, host=host, port=port)
+
