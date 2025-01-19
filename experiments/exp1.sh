1e6fac8c2335ba0feb48a8c2dfce957fa3a3bb41
Fernando Dias <fernando.dias.3741@gmail.com>
2025-01-19 00:38:24 +0000

    Reordered commands to start and finish cleanly on every execution

--- experiments/exp1.sh
+++ experiments/exp1.sh
@@ -9,44 +9,38 @@ mkdir -p logs_${LOG_TIMESTAMP}
 cp experiments/kill_processes.py logs_${LOG_TIMESTAMP}
 
 echo "Start server broker"
-docker stop server-broker-rabbit
-docker rm server-broker-rabbit
-docker run --hostname broker --name server-broker-rabbit -p 9000:5672 rabbitmq:3 > logs_${LOG_TIMESTAMP}/cloud_broker.log 2>&1 &
-sleep 1
+sudo docker run --hostname broker --rm --name server-broker-rabbit -p 9000:5672 rabbitmq:3 > logs_${LOG_TIMESTAMP}/cloud_broker.log 2>&1 &
+sleep 5
 
 echo "Start client broker"
-docker stop client-broker-rabbit
-docker rm client-broker-rabbit
-docker run --hostname broker --name client-broker-rabbit -p 8000:5672 rabbitmq:3 > logs_${LOG_TIMESTAMP}/client_broker.log 2>&1 &
-sleep 10
+sudo docker run --hostname broker --rm --name client-broker-rabbit -p 8000:5672 rabbitmq:3 > logs_${LOG_TIMESTAMP}/client_broker.log 2>&1 &
+sleep 15
 
 echo "Start event reader"
 python -u -m event_reader > logs_${LOG_TIMESTAMP}/event_reader.log 2>&1 &
-echo $! >>  logs_${LOG_TIMESTAMP}/pids
+echo $! >>  pids
 sleep 1
 
 ## CLOUD SIDE
 
 echo "Start cloud gateway"
 python -u -m cloud_gateway.http_gateway > logs_${LOG_TIMESTAMP}/http_gateway.log 2>&1 &
-echo $! >>  logs_${LOG_TIMESTAMP}/pids
+echo $! >>  pids
 sleep 1
 
 echo "Start cloud task manager"
-rm cloud_task_manager/db/tasks.db
 python -u -m cloud_task_manager.service_cloud_ml > logs_${LOG_TIMESTAMP}/service_cloud_ml.log 2>&1 &
-echo $! >>  logs_${LOG_TIMESTAMP}/pids
+echo $! >>  pids
 sleep 1
 
 echo "Start download server"
 python -u -m cloud_task_manager.host_tasks $(pwd)/cloud_task_manager &
-echo $! >>  logs_${LOG_TIMESTAMP}/pids
+echo $! >>  pids
 sleep 1
 
 echo "Start user manager"
-rm user_manager/db/users.db
 python -u -m user_manager.service_user_manager > logs_${LOG_TIMESTAMP}/service_user_manager.log 2>&1 &
-echo $! >>  logs_${LOG_TIMESTAMP}/pids
+echo $! >>  pids
 sleep 1
 
 echo "Create and run task at the server"
@@ -57,31 +51,34 @@ sleep 5
 
 echo "Start client gateway"
 python -u -m client_gateway.amqp_gateway > logs_${LOG_TIMESTAMP}/amqp_gateway.log 2>&1 &
-echo $! >>  logs_${LOG_TIMESTAMP}/pids
+echo $! >>  pids
 sleep 1
 
 echo "Start client tasks manager" 
-rm -rf client_task_manager/tasks/*
-rm -rf client_task_manager/client_info/*
 python -u -m client_task_manager.service_client_ml > logs_${LOG_TIMESTAMP}/service_client_ml.log 2>&1 &
-echo $! >>  logs_${LOG_TIMESTAMP}/pids
+echo $! >>  pids
 sleep 10
 
 echo "Start second Flower client, which will trigger FL to starts"
 python -u -m cloud_task_manager.tasks.task_4fe5.client cli
-echo $! >>  logs_${LOG_TIMESTAMP}/pids
+echo $! >>  pids
 sleep 5
 
 ## FINALIZATION
 
-cd logs_${LOG_TIMESTAMP}
-python kill_processes.py
+sudo docker stop server-broker-rabbit
+
+sudo docker stop client-broker-rabbit
+
+rm cloud_task_manager/db/tasks.db
+
+rm -rf client_task_manager/tasks/*
+rm -rf client_task_manager/client_info/*
+
+rm user_manager/db/users.db
+
+python3 experiments/kill_processes.py
+rm pids
 sleep 5
 
-cd ..
-cp events.json logs_${LOG_TIMESTAMP}
-cp events.json experiments
-
-cd experiments
-echo "---- RESULTS ----" >> exp1_raw_times
-python exp1_process_results.py >> exp1_raw_times
+mv events.json logs_${LOG_TIMESTAMP}
